0f95e75be3f77a7b7f0d3c22c8fa0da033ec7503
prasanth844 <135524731+prasanth844@users.noreply.github.com>
2025-12-07 16:51:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let count = 1;
 
+// Add list items
+function addItems(num) {
+    for (let i = 0; i < num; i++) {
+        const li = document.createElement("li");
+        li.textContent = "Item " + count++;
+        list.appendChild(li);
+    }
+}
+
+// Add initial 10 items
+addItems(10);
+
+// Detect scroll end
+window.addEventListener("scroll", () => {
+    const reachedBottom = 
+        window.innerHeight + window.scrollY >= document.documentElement.scrollHeight;
+
+    if (reachedBottom) {
+        addItems(2);
+    }
+});
